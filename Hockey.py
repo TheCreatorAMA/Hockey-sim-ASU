313348c91830ed219d67b7a2466199354b716db4
Alex <Alexanderakiu@yahoo.com>
2020-04-23 13:59:06 -0700

    Last test upload

--- Hockey.py
+++ Hockey.py
@@ -1,3 +1,12 @@
 import numpy as np
 
-print('Hello')
+#Hockey player class
+class Player():
+	def __inti__(self,age):
+		self.age = age
+
+#main function to run all sub functions
+def main():
+	pass
+
+if __name__ == '__main__': main()
